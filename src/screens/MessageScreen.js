dd4a558cfea931eadc03fb546bd3691aa7c149fa
nganbph10804 <nganbph10804@fpt.edu.vn>
2021-10-16 13:01:58 +0700

    update

--- src/screens/MessageScreen.js
+++ src/screens/MessageScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, FlatList, Alert, TouchableOpacity } from 'react-native';
-import { Searchbar } from 'react-native-paper';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, ViewBase } from 'react-native';
+import { Searchbar, TextInput } from 'react-native-paper';
 import {
     Container,
     Card,
@@ -17,10 +17,16 @@ import SockJS from 'sockjs-client';
 import { useSelector } from 'react-redux';
 import Stomp from 'webstomp-client'
 import { Ionicons } from '@expo/vector-icons';
+import { Button,Paragraph, Dialog, Portal, Provider } from 'react-native-paper';
 
 
 
 const MessagesScreen = ({ navigation }) => {
+    let urlWs = "http://34.67.241.66:8080/ws/";
+    let socket = new SockJS(urlWs);
+    var stompClient = Stomp.over(socket);
+
+    var stompClientConversation = null;
     const { cover, avatar, address, birthday, bio, name, id } = useSelector(
         state => state.auth.profile
     );
@@ -29,12 +35,21 @@ const MessagesScreen = ({ navigation }) => {
         return time && time;
     }
 
-
-
+    
+    const [cvsName, setCvsName] = useState('');
+    const [cvsId, setCvsId] = useState('')
     const [messages, setMessages] = useState([]);
     const [lstMess, setLstMess] = useState([]);
-    var stompClient = null;
-    var stompClientConversation = null;
+    const [visible, setVisible] = useState(false);
+    
+    const clearInput = ()=>{
+        setCvsName('');
+        setCvsId('');
+    }
+    const connect = () => {
+        stompClient.connect({}, () => { subcribeConversation(id) }, error => { console.log(error); });
+    }
+    
 
     const bindingData = (payload) => {
         let body = JSON.parse(payload.body);
@@ -43,6 +58,10 @@ const MessagesScreen = ({ navigation }) => {
                 setLstMess(body.data);
             }
 
+        } else if (body.type == "CREATE_CONVERSATION") {
+            if (body.status == true) {
+               setLstMess(body.data);
+            } 
         }
     }
     const onOpenChat = (item) => {
@@ -65,21 +84,56 @@ const MessagesScreen = ({ navigation }) => {
         stompClient.send(`/app/conversation_${idAccount}/get-list-conversation`, JSON.stringify(objSend), {});
     }
 
-    const connect = () => {
-        let urlWs = "http://34.67.241.66:8080/ws/";
-        let socket = new SockJS(urlWs);
-        stompClient = Stomp.over(socket);
-        stompClient.connect({}, () => { subcribeConversation(id) }, error => { console.log(error); });
+   
+    const onCreateMess = () => {
+
+      
+        let objSend = {
+            "accountId": id,
+            "conversationId": "",
+            "name": cvsName,
+            "accountIdAdd": cvsId
+        };
+        stompClient.send(`/app/conversation_${id}/create-conversation`,  JSON.stringify(objSend),{});
+        hideDialog();
+        clearInput();
+        subcribeConversation(id);
     }
     useEffect(() => {
         connect();
         return () => {
             stompClient && stompClient.disconnect();
         }
-    }, [id]);
+    }, [stompClientConversation]);
+
+    const hideDialog = () => setVisible(false);
 
     return (
+        <Provider>
         <Container>
+            <Portal>
+            <Dialog visible={visible} onDismiss={hideDialog}>
+                <Dialog.Title>Create New Conversation</Dialog.Title>
+                <Dialog.Content>
+                    <Paragraph>name</Paragraph>
+                    <TextInput 
+                    value={cvsName}
+                    onChangeText={setCvsName}
+                    style={styles.inputt}
+                    />
+                    <Paragraph>ID</Paragraph>
+                    <TextInput 
+                    value={cvsId}
+                    onChangeText={setCvsId}
+                    style={styles.inputt}
+                    />
+                </Dialog.Content>
+                <Dialog.Actions>
+                    <Button onPress={hideDialog}>Cancel</Button>
+                    <Button onPress={()=>{onCreateMess()}}>OK</Button>
+                </Dialog.Actions>
+            </Dialog>
+            </Portal>
             <View style={styles.container}>
                 <Searchbar style={styles.searchBarr}
                     inputStyle={{
@@ -90,9 +144,9 @@ const MessagesScreen = ({ navigation }) => {
                 />
 
                 <TouchableOpacity
-                style={styles.createBtn}
+                    style={styles.createBtn}
                     onPress={() => {
-
+                       setVisible(true)
                     }}
                 >
                     <Ionicons
@@ -107,7 +161,7 @@ const MessagesScreen = ({ navigation }) => {
 
             <FlatList
                 data={lstMess}
-                keyExtractor={item => item.id}
+                keyExtractor={item => item.id.toString()}
                 renderItem={({ item }) => (
                     <Card onPress={() => onOpenChat(item)}>
                         <UserInfo>
@@ -117,7 +171,7 @@ const MessagesScreen = ({ navigation }) => {
                             <TextSection>
                                 <UserInfoText>
                                     <UserName>{item.title}</UserName>
-                                    <PostTime>{toTimestamp(item.createBy.updateAt)}</PostTime>
+                                    <PostTime>{toTimestamp(item.createAt)}</PostTime>
                                 </UserInfoText>
                                 <MessageText>{`Touch to open Message`}</MessageText>
                             </TextSection>
@@ -126,6 +180,7 @@ const MessagesScreen = ({ navigation }) => {
                 )}
             />
         </Container>
+        </Provider>
     );
 };
 
@@ -133,20 +188,23 @@ export default MessagesScreen;
 
 const styles = StyleSheet.create({
     container: {
-        display:'flex',
-        alignItems: 'baseline',
-        justifyContent: 'space-between',
-        flexDirection:'row'
+        justifyContent: 'center',
+        flexDirection: 'row'
     },
     searchBarr: {
         marginVertical: 10,
         borderRadius: 20,
+        marginLeft: 15,
         elevation: 0,
         backgroundColor: '#ebebebeb',
         height: 33,
-        width:'90%'
+        width: '90%'
     },
-    createBtn:{
-        marginLeft:20
+    createBtn: {
+        justifyContent: 'center',
+        marginLeft: 15
+    },
+    inputt:{
+        height:30,
     }
 });
